a199ce8791faa51f53feedefc49a1a72f2242cd0
tamim008bd <tamim008bd@gmail.com>
2022-01-07 14:29:22 +0600

    add news card item  design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react'
 import { newsCategory } from './news'
 import Header from './components/header'
+import NewsList from './components/newsList'
+
+const fakeNews = [
+  {
+    title:'Title',
+    content: 'Content',
+    url: 'https:://linktonews.com',
+    urlToImage: 'https://linktoimage.com',
+    publishedAt: 'published date and time',
+    source:{
+      name:'CNN',
+    },
+  },
+  {
+    title:'Title',
+    content: 'Content',
+    url: 'https:://linktonews.com',
+    urlToImage: 'https://linktoimage.com',
+    publishedAt: 'published date and time',
+    source:{
+      name:'CNN',
+    },
+  },
+]
 export class App extends Component {
   render() {
     return (
@@ -8,6 +32,7 @@ export class App extends Component {
           <div className='row'>
             <div className='col-sm-6 offset-md-3'>
                 <Header category = {newsCategory.technology}/>
+                <NewsList news={fakeNews}/>
             </div>
           </div>
       </div>
